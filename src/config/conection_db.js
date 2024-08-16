e0c8568e3ed7fb7222df58b737cc05ba14ad4761
Brayan ramirez <84251018+Brianramirez27@users.noreply.github.com>
2024-08-16 17:41:53 -0500

    .

--- src/config/conection_db.js
+++ src/config/conection_db.js
@@ -1,12 +1,14 @@
 
 import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = new pg.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'FotoStudio',
-    password: 'postgres',
-    port: 5432
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    port: process.env.DB_PORT || 5432
 })
 
 
